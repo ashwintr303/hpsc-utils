19130b277f856fd55553a9573199c97e5d7ff7e8
Alexei Colin <acolin@isi.edu>
2018-12-03 10:13:38 -0500

    wdt-kicker: script to kick HPPS watchdog timers

---
+++ wdt-kicker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MASKS=(0x1 0x2 0x4 0x8 0x10 0x20 0x40 0x80)
+
+while true
+do
+	for i in $(seq 0 7)
+	do
+		taskset ${MASKS[$i]} bash -c "echo 1 > /dev/watchdog$i"
+	done
+	sleep 2
+done
