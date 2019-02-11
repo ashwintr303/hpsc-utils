ceed21449a1ed168303c76be32edf0b20fcd8839
Alexei Colin <acolin@isi.edu>
2019-02-11 11:33:39 -0500

    metrics: script to dump Tech Performance Metrics

---
+++ metrics/metrics.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+run() {
+    echo $@
+    "$@"
+}
+
+elf() {
+    run size "$1"
+}
+
+bin() {
+    run stat -c '%s' "$1"
+}
+
+elf hpsc-baremetal/trch/bld/trch.elf
+bin hpsc-baremetal/trch/bld/trch.elf
+elf hpsc-baremetal/rtps/bld/rtps.elf
+bin hpsc-baremetal/rtps/bld/rtps.elf
+
+elf u-boot-a53/u-boot
+bin u-boot-a53/u-boot.bin
+elf u-boot-r52/u-boot
+bin u-boot-r52/u-boot.bin
+
+elf arm-trusted-firmware/build/hpsc/release/bl31/bl31.elf
+bin arm-trusted-firmware/build/hpsc/release/bl31.bin
+
+elf linux-hpsc/vmlinux
+bin linux-hpsc/arch/arm64/boot/Image.gz
+
+bin linux-hpsc/arch/arm64/boot/dts/hpsc/hpsc.dtb
+bin u-boot-a53/arch/arm/dts/hpsc-hpps.dtb
+bin u-boot-r52/arch/arm/dts/hpsc-rtps-r52.dtb
