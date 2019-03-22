c3b7c9ef48ced344b9cc1385dc28d51e5e061f8a
Alexei Colin <acolin@isi.edu>
2019-03-22 09:56:13 -0700

    collection and extraction of ftrace

---
+++ conf/qemu-env-ftrace-extractor.sh
@@ -0,0 +1,8 @@
+HPPS_INITRAMFS=$HPPS_BIN/ftrace-extractor/initramfs.uimg
+HPPS_RAMOOPS=hpps-ramoops.bin
+
+# Must match reserved memory nodes in Linux device tree
+HPPS_RAMOOPS_ADDR=0x83c00000
+HPPS_RAMOOPS_SIZE=0x400000
+
+HPPS_GPBUF_ADDR=0x84000000
