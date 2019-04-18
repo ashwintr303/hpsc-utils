5ce21930800dae16da831bb6639fcab64a0c3071
Alexei Colin <acolin@isi.edu>
2019-04-18 13:47:42 -0400

    conf: base: hpps: initramfs create /proc

--- conf/base/hpps/initramfs.sh
+++ conf/base/hpps/initramfs.sh
@@ -3,6 +3,6 @@
 # This script perorms the parts of the initramfs creation that require fakeroot
 
 # Create dirs here only because git can't hold empty dirs
-mkdir -p dev
+mkdir -p dev proc
 
 mknod dev/mem c 1 1
