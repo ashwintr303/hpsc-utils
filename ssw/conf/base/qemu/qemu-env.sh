956e24284d31ca875a1099a30030318473f916ca
Alexei Colin <acolin@isi.edu>
2019-04-22 11:08:14 -0400

    ssw: make,conf: rename preload map
    Simplify

--- ssw/conf/base/qemu/qemu-env.sh
+++ ssw/conf/base/qemu/qemu-env.sh
@@ -2,7 +2,7 @@ PROF_DIR=$ROOT/bld/prof/$PROF
 
 PROF_QEMU=$PROF_DIR/qemu
 QEMU_DT_FILE=$PROF_QEMU/prof.qemu.dtb
-MEMORY_FILE=$PROF_QEMU/prof.preload.mem.map
+MEMORY_FILE=$PROF_QEMU/preload.mem.map
 
 PROF_TRCH=$PROF_DIR/trch
 TRCH_SMC_SRAM=$PROF_TRCH/prof.sram.mem.bin
