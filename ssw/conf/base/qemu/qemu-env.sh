86c3648277435b89d994f7b5ca97d88593b0eec2
Alexei Colin <acolin@isi.edu>
2019-04-22 11:22:17 -0400

    ssw: make,conf: rename qemu DT and dfg artifacts

--- ssw/conf/base/qemu/qemu-env.sh
+++ ssw/conf/base/qemu/qemu-env.sh
@@ -1,7 +1,7 @@
 PROF_DIR=$ROOT/bld/prof/$PROF
 
 PROF_QEMU=$PROF_DIR/qemu
-QEMU_DT_FILE=$PROF_QEMU/prof.qemu.dtb
+QEMU_DT_FILE=$PROF_QEMU/qemu.dtb
 MEMORY_FILE=$PROF_QEMU/preload.mem.map
 
 PROF_TRCH=$PROF_DIR/trch
