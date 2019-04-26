542cd2d4ddf24b90b8d08c540bb3374591e2c882
Alexei Colin <acolin@isi.edu>
2019-04-26 18:14:13 -0400

    ssw: conf: nand-writer profile

---
+++ ssw/conf/prof/nand-writer.mk
@@ -0,0 +1,5 @@
+include $(CONF)/overlay/dram-boot/Makefile
+include $(CONF)/overlay/bm-minimal/Makefile
+include $(CONF)/overlay/hpps-ddr-high/Makefile
+include $(CONF)/overlay/hpps-linux-gpmem/Makefile
+include $(CONF)/overlay/nand-writer/Makefile
