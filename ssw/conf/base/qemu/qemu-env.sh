8b2589772486d77b703031d11d6dca895215c47c
Alexei Colin <acolin@isi.edu>
2019-04-26 18:10:30 -0400

    ssw: make: autogen rules from preload mem map

--- ssw/conf/base/qemu/qemu-env.sh
+++ ssw/conf/base/qemu/qemu-env.sh
@@ -2,7 +2,7 @@ PROF_DIR=$ROOT/bld/prof/$PROF
 
 PROF_QEMU=$PROF_DIR/qemu
 QEMU_DT_FILE=$PROF_QEMU/hpsc-arch.dtb
-MEMORY_FILE=$PROF_QEMU/preload.mem.map
+MEMORY_FILE=$PROF_QEMU/preload.prof.mem.map
 
 PROF_TRCH=$PROF_DIR/trch
 TRCH_SMC_SRAM=$PROF_TRCH/prof.sram.mem.bin
