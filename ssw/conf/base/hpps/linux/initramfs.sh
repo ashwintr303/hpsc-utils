4c2ef038cdb584aef351a04be28fb611f9dc8a4f
Alexei Colin <acolin@isi.edu>
2019-05-01 14:09:23 -0400

    ssw: conf: base: create /mnt/root dir

--- ssw/conf/base/hpps/linux/initramfs.sh
+++ ssw/conf/base/hpps/linux/initramfs.sh
@@ -3,7 +3,7 @@
 # This script perorms the parts of the initramfs creation that require fakeroot
 
 # Create dirs here only because git can't hold empty dirs
-mkdir -p dev proc
+mkdir -p dev proc mnt/root
 
 mknod dev/mem c 1 1
 mknod dev/mtdblock0 b 31 0 # NAND
