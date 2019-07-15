bdb600e1359d345e2fb2438284e15fa01f26ee1b
Alexei Colin <acolin@isi.edu>
2019-07-15 18:05:24 -0400

    conf: bm config: fix whitespace
    Tabstop = 8

--- conf/bm-minimal/rtps/bm.config.mk
+++ conf/bm-minimal/rtps/bm.config.mk
@@ -1,4 +1,4 @@
 CONFIG_GTIMER 				= 1
 CONFIG_SLEEP_TIMER 			= 1 # implement sleep() using a timer
-CONFIG_WDT 					= 0
-CONFIG_HPPS_RTPS_MAILBOX  	= 0
+CONFIG_WDT 				= 0
+CONFIG_HPPS_RTPS_MAILBOX  		= 0
