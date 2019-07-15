9498a589496604b8c30b34d9f7409408d43bbe04
Alexei Colin <acolin@isi.edu>
2019-07-15 18:05:24 -0400

    conf: bm config: fix whitespace
    Tabstop = 8

--- conf/test/rtps/bm.config.mk
+++ conf/test/rtps/bm.config.mk
@@ -1,9 +1,9 @@
-TEST_FLOAT 					= 1
-TEST_SORT 					= 1
-TEST_GTIMER					= 1
-TEST_WDT					= 1 # requires CONFIG_RTPS_R52_WDT in trch/Makefile
+TEST_FLOAT 				= 1
+TEST_SORT 				= 1
+TEST_GTIMER				= 1
+TEST_WDT				= 1 # requires CONFIG_RTPS_R52_WDT in trch/Makefile
 TEST_RTI_TIMER				= 1
-TEST_RTPS_TRCH_MAILBOX		= 1
+TEST_RTPS_TRCH_MAILBOX			= 1
 TEST_RTPS_DMA 				= 1
 TEST_RTPS_DMA_CB 			= 1
 TEST_RTPS_MMU 				= 1
