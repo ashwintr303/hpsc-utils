ad844e729e184dd11567246989e44036832ff852
Kaushik Datta <kdatta@isi.edu>
2019-09-13 11:56:29 -0400

    pytest: Minor cleanup to test_timer_interrupt.py

--- test/pytest/test_timer_interrupt.py
+++ test/pytest/test_timer_interrupt.py
@@ -3,6 +3,7 @@ import pytest
 
 local_dir = '../linux/'
 testers = ["rtit-tester"]
+masks = ["0x1", "0x2", "0x4", "0x8", "0x10", "0x20", "0x40", "0x80"]
 
 @pytest.fixture(scope="function")  # scope could be "function", "class", "module", "package", or "session"
 def fixture(host):
@@ -17,10 +18,7 @@ def run_tester_on_host(hostname, tester_num, tester_pre_args, tester_post_args):
     out = run(['ssh', hostname] + tester_pre_args + [tester_remote_path] + tester_post_args, capture_output=True)
     return out
     
-def test_rti_timer_on_each_core(fixture, host):
-    masks = ["0x1", "0x2", "0x4", "0x8", "0x10", "0x20", "0x40", "0x80"]
-    interval_ms = 2000
-
+def test_rti_timer_success_on_each_core(fixture, host):
     for i in range(8):
-        out = run_tester_on_host(host, 0, ['taskset', masks[i]], ["/dev/rti_timer" + str(i), str(interval_ms)])
+        out = run_tester_on_host(host, 0, ['taskset', masks[i]], ["/dev/rti_timer" + str(i), str(2000)])
         assert out.returncode == 0
