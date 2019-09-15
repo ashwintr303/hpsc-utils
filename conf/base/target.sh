11d333c4c8c7d73f9ee43f4182c380c9ddf415d8
Alexei Colin <acolin@isi.edu>
2019-09-14 18:10:52 -0700

    conf: base: target: add LSIO offchip mem
    And homogenize parameters. Also, decrease size of LSIO NAND to not waste
    much time on creation, since it's not used yet.

--- conf/base/target.sh
+++ conf/base/target.sh
@@ -12,6 +12,8 @@
 # something supported by all targets, or (B) override variables defined here in
 # the respective target's env.sh.
 
+# Off-chip memory config
+#
 # Note: With this base profile no source image is specified for any of the
 # memory images, so by default blank images will be created. Profiles that wish
 # to load the image from file, need to override the [src_file] property with
@@ -22,37 +24,55 @@
 # unlikely), nor in targets' env.sh (unless the same file should be loaded by
 # the given target in all configurations -- unlikely), but in individual
 # profiles (that have to do with storing something in the given NV memory).
-
-# Memory connected to HPPS SMC SRAM ports
-
+#
 # All size values are in bytes, not bits
+
+LSIO_SMC_SRAM_SIZE=64M
+LSIO_SMC_SRAM_0+=( [size]=$LSIO_SMC_SRAM_SIZE )
+LSIO_SMC_SRAM_1+=( [size]=$LSIO_SMC_SRAM_SIZE )
+LSIO_SMC_SRAM_2+=( [size]=$LSIO_SMC_SRAM_SIZE )
+LSIO_SMC_SRAM_3+=( [size]=$LSIO_SMC_SRAM_SIZE )
+
 HPPS_SMC_SRAM_SIZE=16K
 HPPS_SMC_SRAM_0+=( [size]=$HPPS_SMC_SRAM_SIZE )
 HPPS_SMC_SRAM_1+=( [size]=$HPPS_SMC_SRAM_SIZE )
 HPPS_SMC_SRAM_2+=( [size]=$HPPS_SMC_SRAM_SIZE )
 HPPS_SMC_SRAM_3+=( [size]=$HPPS_SMC_SRAM_SIZE )
 
-# Memory connected to HPPS SMC NAND ports
-
 # These NAND layout parameters are usually not HW-dependent and can be chosen
 # here. Also, we choose to apply these same values across all memory chips.
-HPPS_SMC_NAND_PAGE_SIZE=2048 # bytes
-HPPS_SMC_NAND_OOB_SIZE=64 # bytes
-HPPS_SMC_NAND_ECC_SIZE=12 # bytes
-HPPS_SMC_NAND_PAGES_PER_BLOCK=64 # bytes
+SMC_NAND_PAGE_SIZE=2048 # bytes
+SMC_NAND_OOB_SIZE=64 # bytes
+SMC_NAND_ECC_SIZE=12 # bytes
+SMC_NAND_PAGES_PER_BLOCK=64 # bytes
 
 # All size values are in bytes, not bits
+LSIO_SMC_NAND_0+=(
+    [size]=16M
+    [page]=$SMC_NAND_PAGE_SIZE
+    [oob]=$SMC_NAND_OOB_SIZE
+    [ecc]=$SMC_NAND_ECC_SIZE
+    [ppb]=$SMC_NAND_PAGES_PER_BLOCK
+)
+LSIO_SMC_NAND_1+=(
+    [size]=16M
+    [page]=$SMC_NAND_PAGE_SIZE
+    [oob]=$SMC_NAND_OOB_SIZE
+    [ecc]=$SMC_NAND_ECC_SIZE
+    [ppb]=$SMC_NAND_PAGES_PER_BLOCK
+)
+
 HPPS_SMC_NAND_0+=(
     [size]=512M
-    [page]=$HPPS_SMC_NAND_PAGE_SIZE
-    [oob]=$HPPS_SMC_NAND_OOB_SIZE
-    [ecc]=$HPPS_SMC_NAND_ECC_SIZE
-    [ppb]=$HPPS_SMC_NAND_PAGES_PER_BLOCK
+    [page]=$SMC_NAND_PAGE_SIZE
+    [oob]=$SMC_NAND_OOB_SIZE
+    [ecc]=$SMC_NAND_ECC_SIZE
+    [ppb]=$SMC_NAND_PAGES_PER_BLOCK
 )
 HPPS_SMC_NAND_1+=(
     [size]=32M
-    [page]=$HPPS_SMC_NAND_PAGE_SIZE
-    [oob]=$HPPS_SMC_NAND_OOB_SIZE
-    [ecc]=$HPPS_SMC_NAND_ECC_SIZE
-    [ppb]=$HPPS_SMC_NAND_PAGES_PER_BLOCK
+    [page]=$SMC_NAND_PAGE_SIZE
+    [oob]=$SMC_NAND_OOB_SIZE
+    [ecc]=$SMC_NAND_ECC_SIZE
+    [ppb]=$SMC_NAND_PAGES_PER_BLOCK
 )
