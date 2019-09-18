fb1c5bcdfed522c1989c9664de5993ed238ebe78
Kaushik Datta <kdatta@isi.edu>
2019-09-18 10:53:45 -0400

    pytest: Added tests for all notification methods in test_mbox.py

--- test/pytest/test_mbox.py
+++ test/pytest/test_mbox.py
@@ -21,17 +21,51 @@ def run_tester_on_host(hostname, tester_num, tester_args):
 # verify that mboxtester works with the process pinned separately to each
 # HPPS core
 @pytest.mark.timeout(30)
-def test_hpps_to_trch_on_each_core(fixture, host):
+def test_hpps_to_trch_with_no_notification_on_each_core(fixture, host):
     for i in range(8):
-        out = run_tester_on_host(host, 0, ['-c', str(i)])
+        out = run_tester_on_host(host, 0, ['-n', 'none', '-c', str(i)])
         assert out.returncode == 0
 
-# verify that mboxtester works with the process pinned separately to each
-# HPPS core
 @pytest.mark.timeout(30)
-def test_hpps_to_rtps_on_each_core(fixture, host):
+def test_hpps_to_trch_with_select_notification_on_each_core(fixture, host):
     for i in range(8):
-        out = run_tester_on_host(host, 0, ['-o', '/dev/mbox/1/mbox0', '-i', '/dev/mbox/1/mbox1', '-c', str(i)])
+        out = run_tester_on_host(host, 0, ['-n', 'select', '-c', str(i)])
+        assert out.returncode == 0
+
+@pytest.mark.timeout(30)
+def test_hpps_to_trch_with_poll_notification_on_each_core(fixture, host):
+    for i in range(8):
+        out = run_tester_on_host(host, 0, ['-n', 'poll', '-c', str(i)])
+        assert out.returncode == 0
+
+@pytest.mark.timeout(30)
+def test_hpps_to_trch_with_epoll_notification_on_each_core(fixture, host):
+    for i in range(8):
+        out = run_tester_on_host(host, 0, ['-n', 'epoll', '-c', str(i)])
+        assert out.returncode == 0
+
+@pytest.mark.timeout(30)
+def test_hpps_to_rtps_with_no_notification_on_each_core(fixture, host):
+    for i in range(8):
+        out = run_tester_on_host(host, 0, ['-n', 'none', '-o', '/dev/mbox/1/mbox0', '-i', '/dev/mbox/1/mbox1', '-c', str(i)])
+        assert out.returncode == 0
+
+@pytest.mark.timeout(30)
+def test_hpps_to_rtps_with_select_notification_on_each_core(fixture, host):
+    for i in range(8):
+        out = run_tester_on_host(host, 0, ['-n', 'select', '-o', '/dev/mbox/1/mbox0', '-i', '/dev/mbox/1/mbox1', '-c', str(i)])
+        assert out.returncode == 0
+
+@pytest.mark.timeout(30)
+def test_hpps_to_rtps_with_poll_notification_on_each_core(fixture, host):
+    for i in range(8):
+        out = run_tester_on_host(host, 0, ['-n', 'poll', '-o', '/dev/mbox/1/mbox0', '-i', '/dev/mbox/1/mbox1', '-c', str(i)])
+        assert out.returncode == 0
+
+@pytest.mark.timeout(30)
+def test_hpps_to_rtps_with_epoll_notification_on_each_core(fixture, host):
+    for i in range(8):
+        out = run_tester_on_host(host, 0, ['-n', 'epoll', '-o', '/dev/mbox/1/mbox0', '-i', '/dev/mbox/1/mbox1', '-c', str(i)])
         assert out.returncode == 0
 
 @pytest.mark.timeout(30)
