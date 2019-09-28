c11559572ff92c8f9c660c1a80bcf89c9c9aa5f6
Alexei Colin <acolin@isi.edu>
2019-09-28 13:37:24 -0400

    conf: lib-hpps-linux: move kernel in memory
    To give more space for latest Yocto initramfs image that's ~20MB big.

--- conf/lib-hpps-linux/hpps/conf.sh
+++ conf/lib-hpps-linux/hpps/conf.sh
@@ -1,4 +1,4 @@
 # Some params needed by the build but not part of any memory map file
 
-HPPS_KERN_LOAD_ADDR=0x81680000 # (base + TEXT_OFFSET), where base must be aligned to 2MB
+HPPS_KERN_LOAD_ADDR=0x81a80000 # (base + TEXT_OFFSET), where base must be aligned to 2MB
 HPPS_UBOOT_ENV_SIZE=0x1000 # must match CONFIG_ENV_SIZE in u-boot config
