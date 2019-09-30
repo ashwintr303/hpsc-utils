8dc2526ef0e18ff1e9ae08052212267f90e1a211
Alexei Colin <acolin@isi.edu>
2019-09-30 11:08:27 -0400

    prof: tool-nand-writer: offset the output
    Want to use dd with a block size that does not align with the
    buffer address.

--- prof/tool-nand-writer/conf.sh
+++ prof/tool-nand-writer/conf.sh
@@ -3,8 +3,9 @@ source $CONF/tool-nand-writer/qemu/env.sh # To give makefile access to QMP_PORT
 
 # Must match settings in initramfs/10-rootfs script from this profile
 CPU=$HPPS_GPBUF_CPU
-SIZE_ADDR=$(($HPPS_GPBUF_ADDR + 0x0000))
-DONE_ADDR=$(($HPPS_GPBUF_ADDR + 0x0020))
-DATA_ADDR=$(($HPPS_GPBUF_ADDR + 0x1000))
+DONE_ADDR=$((  HPPS_GPBUF_ADDR + 0x0000))
+OFFSET_ADDR=$((HPPS_GPBUF_ADDR + 0x0020))
+SIZE_ADDR=$((  HPPS_GPBUF_ADDR + 0x0040))
+DATA_ADDR=$((  HPPS_GPBUF_ADDR + 0x1000))
 DONE_PATTERN="DONE"
 
