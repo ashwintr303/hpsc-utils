dbf5c4b7b1e7ba26308a1d447dec6b3de27973ac
Alexei Colin <acolin@isi.edu>
2019-09-30 02:20:00 -0400

    prof: lib-hpps-busybox: mount sysfs

--- prof/lib-hpps-busybox/hpps/initramfs.dir.sh
+++ prof/lib-hpps-busybox/hpps/initramfs.dir.sh
@@ -3,7 +3,7 @@
 # This script perorms the parts of the initramfs creation that require fakeroot
 
 # Create dirs here only because git can't hold empty dirs
-mkdir -p dev proc mnt/root
+mkdir -p dev proc sys mnt/root
 
 mknod dev/mem c 1 1
 mknod dev/mtd0 c 90 0 # NAND
