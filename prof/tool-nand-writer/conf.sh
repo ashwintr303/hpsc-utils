2c356bfd9a6d9a239e8bafd98fd99a8100b83f5f
Alexei Colin <acolin@isi.edu>
2019-09-30 02:21:18 -0400

    make,prof: tool-nand-writer: dump image with oob
    Now we support make images for partitions, instead of just whole memory images.

--- prof/tool-nand-writer/conf.sh
+++ prof/tool-nand-writer/conf.sh
@@ -2,6 +2,9 @@ source $CONF/lib-hpps-linux-gpmem/conf.sh
 source $CONF/tool-nand-writer/qemu/env.sh # To give makefile access to QMP_PORT
 
 # Must match settings in initramfs/10-rootfs script from this profile
-DONE_CPU=$HPPS_GPBUF_CPU
-DONE_ADDR=$(($HPPS_GPBUF_ADDR + 0x20))
+CPU=$HPPS_GPBUF_CPU
+SIZE_ADDR=$(($HPPS_GPBUF_ADDR + 0x0000))
+DONE_ADDR=$(($HPPS_GPBUF_ADDR + 0x0020))
+DATA_ADDR=$(($HPPS_GPBUF_ADDR + 0x1000))
 DONE_PATTERN="DONE"
+
