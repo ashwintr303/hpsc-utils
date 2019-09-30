652afc5cc2466bc987daa954bd721b07e32d962c
Alexei Colin <acolin@isi.edu>
2019-09-30 02:11:43 -0400

    make,prof: nand: jffs2 img to page size multiple
    Will simplify tool-nand-writer script.

--- prof/lib-hpps-linux-nand/hpps/rootfs.jffs2.conf.sh
+++ prof/lib-hpps-linux-nand/hpps/rootfs.jffs2.conf.sh
@@ -1,3 +1,4 @@
 # Must match NAND memory chip parameters in prof/platform/*/mem.ini
 # TODO: JFFS2 images are platform-specific, so this needs revisit.
 NAND_ERASE_BLOCK_SIZE_KB=128
+NAND_PAGE_SIZE=2048
