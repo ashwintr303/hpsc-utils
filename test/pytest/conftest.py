972ad823a608c27605f74f4eb292468bf1c23e98
Kaushik Datta <kdatta@isi.edu>
2019-10-21 16:32:22 -0400

    pytest: Add conftest.py (which centralizes the pytest fixture) and pytest.ini

--- test/pytest/conftest.py
+++ test/pytest/conftest.py
@@ -1,10 +1,40 @@
+import serial
+import pexpect
+import pexpect.fdpexpect
+import subprocess
 import pytest
+
+@pytest.fixture(scope="session")
+def boot_qemu():
+    p = subprocess.Popen(["hpsc-bsp/run-qemu.sh -- -S"])
+#    p = subprocess.Popen(["${CODEBUILD_SRC_DIR}/hpsc-bsp/run-qemu.sh"])
+    # ULTIMATELY REMOVE THESE SLEEP CALLS- THEY ARE NOT RELIABLE
+    subprocess.run(['sleep', '30'])
+
+    # connect to the HPPS serial port- HOWEVER, THE PORT SHOULD NOT BE HARD CODED
+    ser = serial.Serial(port='/dev/pts/2', baudrate=115200)
+    subprocess.run(['sleep', '20'])
+    child = pexpect.fdpexpect.fdspawn(ser, timeout=1000)
+
+    # USE A TRY BLOCK FOR THE FOLLOWING, QMP PORT NUMBER SHOULD NOT BE HARD CODED
+    subprocess.run(['python3', 'hpsc-bsp/sdk/tools/qmp.py', 'localhost', '2024', 'cont'])
+#    subprocess.run(["python3", "${CODEBUILD_SRC_DIR}/hpsc-bsp/sdk/tools/qmp.py", "localhost", "2024", "cont"])
+
+    # log into the HPPS, then close the HPPS serial port
+    child.expect("hpsc-chiplet login: ")
+#    print('child.before 1 = ', child.before)
+    child.sendline('root')
+    child.expect('root@hpsc-chiplet:~# ')
+#    print('child.before 2 = ', child.before)
+    ser.close()
+    yield boot_qemu
+    p.terminate()
 
 def pytest_addoption(parser):
     parser.addoption("--host", action="store", help="host")
 
 def pytest_generate_tests(metafunc):
-    # This is called for every test
+    # this is called for every test
     option_value = metafunc.config.option.host
     if 'host' in metafunc.fixturenames and option_value is not None:
         metafunc.parametrize("host", [option_value])
