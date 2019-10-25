15b6e91403b9b3f312bd0f0800ddd991b106afc2
Kaushik Datta <kdatta@isi.edu>
2019-10-25 17:40:53 -0400

    pytest: Modified test_dma.py to make it up-to-date with current pytest fixtures

--- test/pytest/test_dma.py
+++ test/pytest/test_dma.py
@@ -1,42 +1,41 @@
-from subprocess import run
+import subprocess
 import pytest
 
-local_dir = '../linux/'
 testers = ["dma-tester.sh"]
 
-@pytest.fixture(scope="function")  # scope could be "function", "class", "module", "package", or "session"
-def fixture(host):
-    local_tester_paths = [local_dir + t for t in testers]
-    run(['scp'] + local_tester_paths + [host + ':~'])
-    yield fixture # provide the fixture value, then start teardown code
-    remote_tester_paths = ["~/" + t for t in testers]
-    run(['ssh', host, 'rm'] + remote_tester_paths)
-
-def run_tester_on_host(hostname, tester_num, tester_args):
-    tester_remote_path = "~/" + testers[tester_num]
-    out = run(['ssh', hostname, tester_remote_path] + tester_args, capture_output=True)
+# Since this first test may need to boot QEMU, it is given more time.
+@pytest.mark.timeout(200)
+def run_tester_on_host(hostname, tester_num, tester_pre_args, tester_post_args):
+    tester_remote_path = "/opt/hpsc-utils/" + testers[tester_num]
+    out = subprocess.run(['ssh', hostname] + tester_pre_args + [tester_remote_path] + tester_post_args, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return out
-    
-def test_all_dma_channels(fixture, host):
-    out = run_tester_on_host(host, 0, [])
+
+@pytest.mark.timeout(60)
+def test_all_dma_channels(boot_qemu, host):
+    out = run_tester_on_host(host, 0, [], [])
     assert out.returncode == 0
 
-def test_invalid_test_buffer_size(fixture, host):
-    out = run_tester_on_host(host, 0, ['-b', '-1'])
+@pytest.mark.timeout(60)
+def test_invalid_test_buffer_size(boot_qemu, host):
+    out = run_tester_on_host(host, 0, [], ['-b', '-1'])
     assert out.returncode == 1
 
-def test_invalid_threads_per_channel(fixture, host):
-    out = run_tester_on_host(host, 0, ['-h', '-1'])
+@pytest.mark.timeout(60)
+def test_invalid_threads_per_channel(boot_qemu, host):
+    out = run_tester_on_host(host, 0, [], ['-h', '-1'])
     assert out.returncode == 2
 
-def test_invalid_iterations(fixture, host):
-    out = run_tester_on_host(host, 0, ['-i', '-1'])
+@pytest.mark.timeout(60)
+def test_invalid_iterations(boot_qemu, host):
+    out = run_tester_on_host(host, 0, [], ['-i', '-1'])
     assert out.returncode == 3
 
-def test_invalid_timeout(fixture, host):
-    out = run_tester_on_host(host, 0, ['-t', '-1'])
+@pytest.mark.timeout(60)
+def test_invalid_timeout(boot_qemu, host):
+    out = run_tester_on_host(host, 0, [], ['-t', '-1'])
     assert out.returncode == 4
 
-def test_invalid_channel(fixture, host):
-    out = run_tester_on_host(host, 0, ['-c', 'nochan'])
+@pytest.mark.timeout(60)
+def test_invalid_channel(boot_qemu, host):
+    out = run_tester_on_host(host, 0, [], ['-c', 'nochan'])
     assert out.returncode == 5
