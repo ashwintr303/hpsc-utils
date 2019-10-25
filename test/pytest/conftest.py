86a3e13c7a95e5072839278ff72646cf5fa31d3b
Kaushik Datta <kdatta@isi.edu>
2019-10-25 11:13:35 -0400

    pytest: Modified conftest.py to run on AWS CodeBuild

--- test/pytest/conftest.py
+++ test/pytest/conftest.py
@@ -6,8 +6,8 @@ import pytest
 
 @pytest.fixture(scope="session")
 def boot_qemu():
-    p = subprocess.Popen(["hpsc-bsp/run-qemu.sh -- -S"])
-#    p = subprocess.Popen(["${CODEBUILD_SRC_DIR}/hpsc-bsp/run-qemu.sh"])
+    # THE FOLLOWING IS SPECIFICALLY FOR AWS CODEBUILD- IT SHOULD BE GENERALIZED
+    p = subprocess.Popen(['${CODEBUILD_SRC_DIR}/hpsc-bsp/run-qemu.sh', '--', '-S'])
     # ULTIMATELY REMOVE THESE SLEEP CALLS- THEY ARE NOT RELIABLE
     subprocess.run(['sleep', '30'])
 
