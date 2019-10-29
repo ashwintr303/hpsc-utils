34a4fc1669b302bc8312d1e5ad7618a3449dc84c
Alexei Colin <acolin@isi.edu>
2019-10-29 15:59:15 -0400

    prof: lib-trch-bm: serial now disabled by default

--- prof/lib-trch-bm/qemu/env.sh
+++ prof/lib-trch-bm/qemu/env.sh
@@ -1,7 +1,1 @@
-# Profiles may disable by setting the respective key to empty (unsetting the
-# key instead of setting to empty applies the default in the launch-* script).
-SERIAL_PORT_NAMES=(
-    [serial0]="trch"
-    [serial1]=""
-    [serial2]=""
-)
+SERIAL_PORT_NAMES[serial0]="trch"
