d4464a2ad467d75b2fd1e2ef528acc4393106b8a
Alexei Colin <acolin@isi.edu>
2019-11-01 19:48:52 -0400

    prof: rtps r52: profiles for SPLIT and SMP modes

---
+++ prof/sys-preload-trch-bm-min-r52-split-0-uboot-1-bm-min/qemu/env.sh
@@ -0,0 +1,5 @@
+#   Option A: TRCH=LSIO 0, R52-core0: LSIO 1, R52-core1: HPPS
+SERIAL_PORT_NAMES[serial2]="hpps"
+
+#   Option B: TRCH=no uart, R52-core0: LSIO 1, R52-core1: LSIO 0
+# nothing to override, rely on default
---
+++ prof/sys-preload-trch-bm-min-r52-split-0-uboot-1-bm-min/trch/bm.config.mk
@@ -0,0 +1,5 @@
+# Option A: TRCH=LSIO 0, R52-core0: LSIO 1, R52-core1: HPPS
+# nothing to override, leave at default
+
+# Option B: TRCH=no uart, R52-core0: LSIO 1, R52-core1: LSIO 0
+# CONFIG_CONSOLE=
