3c59d62519ed14398eae30c6b909067ab2c1fb6a
Alexei Colin <acolin@isi.edu>
2019-11-06 13:11:47 -0500

    prof: atf: specify load address in profile
    So that all addresses are defined together.

--- prof/lib-rtps-a53-uboot/rtps/a53/atf.config.mk
+++ prof/lib-rtps-a53-uboot/rtps/a53/atf.config.mk
@@ -1,2 +1,6 @@
 # DEBUG needs override, other vars shouldn't need it
 override DEBUG:=1
+
+# Top of RTPS DRAM partition that belongs to A53
+HPSC_ATF_MEM_BASE=0x60000000
+HPSC_ATF_MEM_SIZE=0x00020000
