d02c086500fb34b6294a87bf4b65c618724c3ba9
Kaushik Datta <kdatta@isi.edu>
2019-11-15 15:29:12 -0500

    pytest: Modify test_wdt.py to check HPPS serial port

--- test/pytest/test_wdt.py
+++ test/pytest/test_wdt.py
@@ -1,46 +1,18 @@
 import serial
-import pexpect
-import pexpect.fdpexpect
-import subprocess
 import pytest
-from threading import Thread
-from queue import Queue
+from pexpect.fdpexpect import fdspawn
 
 testers = ["wdtester"]
 
-def run_tester_on_host(hostname, tester_num, tester_pre_args, tester_post_args):
-    tester_remote_path = "/opt/hpsc-utils/" + testers[tester_num]
-    # using "Popen" instead of "run" below to make the command non-blocking
-    out = subprocess.Popen(['ssh', hostname] + tester_pre_args + [tester_remote_path] + tester_post_args, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return out
-
-def enqueue_output(out, queue):
-    for line in iter(out.readline, b''):
-        queue.put(line)
-    out.close()
-
-# This test cycles through each core so that each one kicks its own watchdog.
+# Each core starts its own watchdog timer and then kicks it.
 # Since this test boots QEMU, it is given more time.
 @pytest.mark.timeout(200)
 @pytest.mark.parametrize('core_num', range(7))
-def test_kicked_hpsc_watchdog(boot_qemu_per_function, host, core_num):
-    p = run_tester_on_host(host, 0, ['taskset', '-c', str(core_num)], ['/dev/watchdog' + str(core_num), '1'])
-
-    # the queue and thread below are used to check stdout in a non-blocking manner
-    q = Queue()
-    t = Thread(target=enqueue_output, args=(p.stdout, q))
-    t.daemon = True
-    t.start()
-
-    while True:
-        search_str = "Kicking watchdog: yes"
-        try:
-            line = q.get_nowait()
-            print("line=" + line)
-            if (search_str in line):
-                assert (search_str in line)
-                break
-        except:
-            pass
-                
-    p.terminate()
+def test_kicked_watchdog_on_each_core(boot_qemu_per_function, host, core_num):
+    # NEED TO REMOVE HARD-CODED PORTS LIKE BELOW
+    ser = serial.Serial(port='/dev/pts/2', baudrate=115200)
+    child = fdspawn(ser, timeout=1000)
+    child.sendline("taskset -c " + str(core_num) + " /opt/hpsc-utils/wdtester /dev/watchdog" + str(core_num) + " 1")
+    # the expect call below should return a 0 on a successful match
+    assert(child.expect("Kicking watchdog: yes") == 0)
+    ser.close()
