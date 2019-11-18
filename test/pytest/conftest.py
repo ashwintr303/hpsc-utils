b66ed6d19f3083ff72ddcbb29d8f5cad585c8436
Kaushik Datta <kdatta@isi.edu>
2019-11-18 15:58:16 -0500

    pytest: Modify conftest.py so directory is switched before booting QEMU (needed for NAND-based boots)

--- test/pytest/conftest.py
+++ test/pytest/conftest.py
@@ -5,17 +5,21 @@ import subprocess
 import pytest
 import os
 
-# Make sure that the CODEBUILD_SRC_DIR env var is set- on AWS CodeBuild,
-# this is done automatically.  In any other environment, it needs to be set.
+# Make sure that the CODEBUILD_SRC_DIR env var is set.  This is the directory
+# where the hpsc-bsp directory is located.  On AWS CodeBuild, this is done
+# automatically.  In any other environment, it needs to be set.
 
 def boot_qemu():
+    # Change to the hpsc-bsp directory
+    os.chdir(str(os.environ['CODEBUILD_SRC_DIR']) + "/hpsc-bsp")
+
     # Create a file with unspecified port names so that screen sessions to serial ports are disabled
-    f = open(str(os.environ['CODEBUILD_SRC_DIR']) + "/hpsc-bsp/qemu-env-override.sh", "w")
+    f = open("./qemu-env-override.sh", "w")
     f.write("SERIAL_PORT_NAMES[serial0]=\"\"\nSERIAL_PORT_NAMES[serial1]=\"\"\nSERIAL_PORT_NAMES[serial2]=\"\"\n")
     f.close()
 
     # Now start QEMU without any screen sessions
-    p = subprocess.Popen([str(os.environ['CODEBUILD_SRC_DIR']) + "/hpsc-bsp/run-qemu.sh", "-e", str(os.environ['CODEBUILD_SRC_DIR']) + "/hpsc-bsp/qemu-env.sh", "-e", str(os.environ['CODEBUILD_SRC_DIR']) + "/hpsc-bsp/qemu-env-override.sh", "--", "-S"])
+    p = subprocess.Popen(["./run-qemu.sh", "-e", "./qemu-env.sh", "-e", "./qemu-env-override.sh", "--", "-S"])
 
     # ULTIMATELY REMOVE THESE SLEEP CALLS- THEY ARE NOT RELIABLE
     subprocess.run(['sleep', '30'])
@@ -26,7 +30,7 @@ def boot_qemu():
     child = pexpect.fdpexpect.fdspawn(ser, timeout=1000)
 
     # USE A TRY BLOCK FOR THE FOLLOWING, QMP PORT NUMBER SHOULD NOT BE HARD CODED
-    subprocess.run(["python3", str(os.environ['CODEBUILD_SRC_DIR']) + "/hpsc-bsp/sdk/tools/qmp-cmd", "localhost", "2024", "cont"])
+    subprocess.run(["python3", "sdk/tools/qmp-cmd", "localhost", "2024", "cont"])
 
     # log into the HPPS, then close the HPPS serial port
     child.expect("hpsc-chiplet login: ")
