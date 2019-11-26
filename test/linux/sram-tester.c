fc2f386d7d035f2a831001102850fa128e6833dc
Kaushik Datta <kdatta@isi.edu>
2019-11-26 15:07:24 -0500

    test/linux/sram-tester.c: Add increment option

--- test/linux/sram-tester.c
+++ test/linux/sram-tester.c
@@ -45,17 +45,17 @@ static void print_array(int *arr, int num_ints) {
 }
 
 /* Modify the contents of arr by incrementing each element */
-static void modify_array(int *arr, int num_ints) {
+static void modify_array(int *arr, int num_ints, int increment) {
     int i;
 
     for (i = 0; i < num_ints; i++) {
-        arr[i] = arr[i]+1;
+        arr[i] = arr[i]+increment;
     }
 }
 
 /*  If we are modifying SRAM, then arr will be updated to contain
- *  the new contents of SRAM.  Otherwise, print the SRAM contents */ 
-static int access_sram(int num_ints, int is_modify) {
+ *  the new contents of SRAM.  Otherwise, print the SRAM contents */
+static int access_sram(int num_ints, int is_modify, int increment) {
     int fd;
     int file_size;
     int *map;  /* mmapped array of ints */
@@ -79,8 +79,8 @@ static int access_sram(int num_ints, int is_modify) {
     }
 
     if (is_modify) {
-	/* Write to the array to see if the changes are visible later. */
-        modify_array(map, num_ints);
+	/* Modify the array to see if the changes are visible later. */
+        modify_array(map, num_ints, increment);
     }
 
     printf("Latest SRAM contents:\n");
@@ -98,19 +98,22 @@ static int access_sram(int num_ints, int is_modify) {
 
 static void usage(const char *pname, int code) {
     fprintf(code ? stderr : stdout,
-            "Usage: %s [-s SIZE] [-m] [-h]\n"
-            "  -s, --size=SIZE      The array size (in ints) to modify/print (default=" num2str(DEFAULT_SIZE) ")\n"
-            "  -m, --modify         Modify the first SIZE ints in the array\n"
+            "Usage: %s [-s SIZE] [-i N] [-h]\n"
+            "  -s, --size=SIZE      The array size (in ints) to modify/print\n"
+            "                       The default value is " num2str(DEFAULT_SIZE) "\n"
+            "  -i, --increment=N    Increment the first SIZE ints in the array by N\n"
+            "                       N must be an integer, but may be zero or negative\n"
+            "                       Off by default\n"
             "  -h, --help           Print this message and exit\n",
             pname);
     exit(code);
 }
 
-static const char short_options[] = "s:mh";
+static const char short_options[] = "s:i:h";
 static const struct option long_options[] = {
-    {"size",    required_argument,  NULL,   's'},
-    {"modify",  no_argument,        NULL,   'm'},
-    {"help",    no_argument,        NULL,   'h'},
+    {"size",       required_argument,  NULL,   's'},
+    {"increment",  required_argument,  NULL,   'i'},
+    {"help",       no_argument,        NULL,   'h'},
     {0, 0, 0, 0}
 };
 
@@ -118,8 +121,9 @@ int main(int argc, char *argv[]) {
     int c;
 
     // set parameter default values
-    int num_ints = DEFAULT_SIZE;
     int is_modify = 0;
+    int increment;
+    int num_ints = DEFAULT_SIZE;
     int ret = 0;
 
     // parse options
@@ -128,8 +132,9 @@ int main(int argc, char *argv[]) {
         case 's':
             num_ints = atoi(optarg);
             break;
-        case 'm':
+        case 'i':
             is_modify = 1;
+	    increment = atoi(optarg);
             break;
         case 'h':
             usage(argv[0], 0);
@@ -144,6 +149,6 @@ int main(int argc, char *argv[]) {
     }
 
     print_params(num_ints, is_modify);
-    ret = access_sram(num_ints, is_modify);
+    ret = access_sram(num_ints, is_modify, increment);
     return ret;
 }
