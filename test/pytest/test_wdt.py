90b34b7f5c2e8db1243aea8efee5522685e98b31
Kaushik Datta <kdatta@isi.edu>
2019-12-06 14:49:17 -0500

    pytest: Test all cores in test_wdt.py

--- test/pytest/test_wdt.py
+++ test/pytest/test_wdt.py
@@ -7,7 +7,7 @@ testers = ["wdtester"]
 # Each core starts its own watchdog timer and then kicks it.
 # Since this test boots QEMU, it is given more time.
 @pytest.mark.timeout(400)
-@pytest.mark.parametrize('core_num', range(1))
+@pytest.mark.parametrize('core_num', range(8))
 def test_kicked_watchdog_on_each_core(qemu_hpps_ser_conn_per_fcn, host, core_num):
     hpps_ser_fd = fdspawn(qemu_hpps_ser_conn_per_fcn, timeout=1000)
     hpps_ser_fd.sendline("taskset -c " + str(core_num) + " /opt/hpsc-utils/wdtester /dev/watchdog" + str(core_num) + " 1")
@@ -18,7 +18,7 @@ def test_kicked_watchdog_on_each_core(qemu_hpps_ser_conn_per_fcn, host, core_num
 # Each core starts its own watchdog timer but does not kick it.
 # Since this involves a HPPS reboot, it is given more time.
 @pytest.mark.timeout(800)
-@pytest.mark.parametrize('core_num', range(1))
+@pytest.mark.parametrize('core_num', range(8))
 def test_unkicked_watchdog_on_each_core(qemu_hpps_ser_conn_per_fcn, host, core_num):
     hpps_ser_fd = fdspawn(qemu_hpps_ser_conn_per_fcn, timeout=1000)
     hpps_ser_fd.sendline("taskset -c " + str(core_num) + " /opt/hpsc-utils/wdtester /dev/watchdog" + str(core_num) + " 0")
