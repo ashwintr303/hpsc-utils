6ddb4ffe6950ac8987a3daffeff0936d1c2932e2
Kaushik Datta <kdatta@isi.edu>
2019-12-10 15:53:10 -0500

    pytest: Remove use of taskset masks

--- test/pytest/test_interrupt_affinity.py
+++ test/pytest/test_interrupt_affinity.py
@@ -2,7 +2,6 @@ import subprocess
 import pytest
 
 testers = ["interrupt-affinity-tester.sh"]
-masks = ["0x1", "0x2", "0x4", "0x8", "0x10", "0x20", "0x40", "0x80"]
 
 def run_tester_on_host(hostname, tester_num, tester_pre_args, tester_post_args):
     tester_remote_path = "/opt/hpsc-utils/" + testers[tester_num]
@@ -13,5 +12,5 @@ def run_tester_on_host(hostname, tester_num, tester_pre_args, tester_post_args):
 @pytest.mark.timeout(200)
 @pytest.mark.parametrize('core_num', range(8))
 def test_interrupt_affinity_on_each_core(qemu_instance_per_mdl, host, core_num):
-    out = run_tester_on_host(host, 0, ['taskset', masks[core_num]], [str(core_num)])
+    out = run_tester_on_host(host, 0, ['taskset', '-c', str(core_num)], [str(core_num)])
     assert out.returncode == 0
--- test/pytest/test_timer_interrupt.py
+++ test/pytest/test_timer_interrupt.py
@@ -2,7 +2,6 @@ import subprocess
 import pytest
 
 testers = ["rtit-tester"]
-masks = ["0x1", "0x2", "0x4", "0x8", "0x10", "0x20", "0x40", "0x80"]
 
 def run_tester_on_host(hostname, tester_num, tester_pre_args, tester_post_args):
     tester_remote_path = "/opt/hpsc-utils/" + testers[tester_num]
@@ -13,5 +12,5 @@ def run_tester_on_host(hostname, tester_num, tester_pre_args, tester_post_args):
 @pytest.mark.timeout(200)
 @pytest.mark.parametrize('core_num', range(8))
 def test_rti_timer_on_each_core(qemu_instance_per_mdl, host, core_num):
-    out = run_tester_on_host(host, 0, ['taskset', masks[core_num]], ["/dev/rti_timer" + str(core_num), str(2000)])
+    out = run_tester_on_host(host, 0, ['taskset', '-c', str(core_num)], ["/dev/rti_timer" + str(core_num), str(2000)])
     assert out.returncode == 0
