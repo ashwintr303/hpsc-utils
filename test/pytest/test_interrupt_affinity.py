c739778c95ed8dbd83a55798cd7e141e9c9b013e
Kaushik Datta <kdatta@isi.edu>
2019-12-18 14:25:16 -0500

    pytest: Change interrupt affinity test to pass core num

--- test/pytest/test_interrupt_affinity.py
+++ test/pytest/test_interrupt_affinity.py
@@ -12,5 +12,5 @@ def run_tester_on_host(hostname, tester_num, tester_pre_args, tester_post_args):
 @pytest.mark.timeout(200)
 @pytest.mark.parametrize('core_num', range(8))
 def test_interrupt_affinity_on_each_core(qemu_instance_per_mdl, host, core_num):
-    out = run_tester_on_host(host, 0, ['taskset', '-c', str(core_num)], [str(core_num)])
+    out = run_tester_on_host(host, 0, [], ['-c', str(core_num)])
     assert out.returncode == 0
