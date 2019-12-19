309067d56b759489f4bf6c07d1c477c29c7ce6b3
Kaushik Datta <kdatta@isi.edu>
2019-12-19 15:40:37 -0500

    pytest: Improve error reporting and parameter testing in test_dma.py

--- test/pytest/test_dma.py
+++ test/pytest/test_dma.py
@@ -2,6 +2,7 @@ import subprocess
 import pytest
 
 testers = ["dma-tester.sh"]
+fail_str = "\"\\nARGS:\\n\" + str(out.args) + \"\\nRETURN CODE:\\n\" + str(out.returncode) + \"\\nSTDOUT:\\n\" + out.stdout + \"\\nSTDERR:\\n\" + out.stderr"
 
 def run_tester_on_host(hostname, tester_num, tester_pre_args, tester_post_args):
     tester_remote_path = "/opt/hpsc-utils/" + testers[tester_num]
@@ -10,26 +11,40 @@ def run_tester_on_host(hostname, tester_num, tester_pre_args, tester_post_args):
 
 # Since this first test will boot QEMU, it is given more than the default time
 @pytest.mark.timeout(200)
-def test_all_dma_channels(qemu_instance_per_mdl, host):
-    out = run_tester_on_host(host, 0, [], [])
-    assert out.returncode == 0
+@pytest.mark.parametrize('buf_size', [8192, 16384, -1])
+def test_test_buffer_size(qemu_instance_per_mdl, host, buf_size):
+    out = run_tester_on_host(host, 0, [], ['-b', str(buf_size)])
+    if buf_size > 0:
+        assert out.returncode == 0, eval(fail_str)
+    else:
+        assert out.returncode == 1, eval(fail_str)
 
-def test_invalid_test_buffer_size(qemu_instance_per_mdl, host):
-    out = run_tester_on_host(host, 0, [], ['-b', '-1'])
-    assert out.returncode == 1
+@pytest.mark.parametrize('threads_per_chan', [1, 2, 4, -1])
+def test_threads_per_channel(qemu_instance_per_mdl, host, threads_per_chan):
+    out = run_tester_on_host(host, 0, [], ['-T', str(threads_per_chan)])
+    if threads_per_chan > 0:
+        assert out.returncode == 0, eval(fail_str)
+    else:
+        assert out.returncode == 2, eval(fail_str)
 
-def test_invalid_threads_per_channel(qemu_instance_per_mdl, host):
-    out = run_tester_on_host(host, 0, [], ['-T', '-1'])
-    assert out.returncode == 2
+@pytest.mark.parametrize('iterations', [1, 2, -1])
+def test_iterations(qemu_instance_per_mdl, host, iterations):
+    out = run_tester_on_host(host, 0, [], ['-i', str(iterations)])
+    if iterations > 0:
+        assert out.returncode == 0, eval(fail_str)
+    else:
+        assert out.returncode == 3, eval(fail_str)
 
-def test_invalid_iterations(qemu_instance_per_mdl, host):
-    out = run_tester_on_host(host, 0, [], ['-i', '-1'])
-    assert out.returncode == 3
+@pytest.mark.parametrize('timeouts', [1500, 3000, -1])
+def test_timeouts(qemu_instance_per_mdl, host, timeouts):
+    out = run_tester_on_host(host, 0, [], ['-t', str(timeouts)])
+    if timeouts > 0:
+        assert out.returncode == 0, eval(fail_str)
+    else:
+        assert out.returncode == 4, eval(fail_str)
 
-def test_invalid_timeout(qemu_instance_per_mdl, host):
-    out = run_tester_on_host(host, 0, [], ['-t', '-1'])
-    assert out.returncode == 4
-
-def test_invalid_channel(qemu_instance_per_mdl, host):
-    out = run_tester_on_host(host, 0, [], ['-c', 'nochan'])
-    assert out.returncode == 5
+@pytest.mark.parametrize('chan', ["nochan"])
+def test_dma_channels(qemu_instance_per_mdl, host, chan):
+    out = run_tester_on_host(host, 0, [], ['-c', chan])
+    if chan == "nochan":
+        assert out.returncode == 5, eval(fail_str)
